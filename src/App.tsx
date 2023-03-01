30c8644981c0f9f133d5ef649382e8a4d1a9ec83
Mark P <mj.poso18@gmail.com>
2023-03-01 18:01:38 +0800

    Move App component back to its own file

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return (
+    <div className="h-screen bg-neutral-800 text-white grid place-items-center">Hello, world</div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './App.js';
 import './tailwind.css';
-
-function App() {
-  return (
-    <div className="h-screen bg-neutral-800 text-white grid place-items-center">Hello, world!</div>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
